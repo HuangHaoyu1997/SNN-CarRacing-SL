524cb6085991d02a7eb4db6a9c8c33d7a899a5b6
Haoyu Huang <31029753+HuangHaoyu1997@users.noreply.github.com>
2021-02-25 00:22:39 +0800

    finish！！

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 train a SNN model using supervised learning to handle CarRacing problem
 
 We use first a CNN model to train the dataset collected by human playing in CarRacing Game. Then we use a Spike-CNN model to train the dataset and compare performance on two models.
+
+The model trained by dataset collected by human-playing CarRacing Game Simulator is tested on the env of `car_racing.py` and the average score is around 670.
+
+Our dataset is ~80000 images with corresponding action labels. We split the total dataset into training set and testing set on the ratio of 4:1.
--- run.py
+++ run.py
@@ -16,17 +16,21 @@ import random
 import os
 import time
 from model import *
+from car_racing import CarRacing
 import argparse
 
 gym.logger.set_level(40)
+num_epi = 100
+render = False
 
-pretrain_model = torch.load('./checkpoint/cnn-test/cnn-test.pth')
+pretrain_model = torch.load('./checkpoint/CNN-CarRacing/CNN-CarRacing.pth')
 # print(pretrain_model.keys()) # ['net', 'acc', 'epoch', 'acc_record', 'loss_test_record', 'loss_train_record', 'acc_label']
 net = ConvNet(input_channel=3,output_channel=4) # .to(device)
 net.load_state_dict(pretrain_model['net']) # 载入预训练模型
 net.eval() # 设置为推理模式，避免单样本送进网络对batchnorm产生影响
 
-env = gym.make('CarRacing-v0')
+# env = gym.make('CarRacing-v0')
+env = CarRacing()
 
 def action_projection(action):
     '''
@@ -46,30 +50,55 @@ def startup():
     '''
     每局游戏开始之初，执行10~20次加速操作，此后再不加速
     '''
-    random = np.random.randint(10,20)
+    times = np.random.randint(15,16)
     epi_r = 0
-    for i in range(random):
+    for i in range(times):
         st,rt,done,_ = env.step(action_projection(2))
         epi_r += rt
-    return st, epi_r, done, random
+    return st, epi_r, done, times
 
-num_epi = 10
+def repeat():
+    times = np.random.randint(3,5)
+    epi_r = 0
+    for i in range(times):
+        st,rt,done,_ = env.step(action_projection(3))
+        epi_r += rt
+    return st, epi_r, done, times
+
+r_list = []
 for i in range(num_epi):
     st = env.reset()
     done = False
-    st, rt, done, random = startup()
-    epi_r = rt
-    action_count = [0,0,random,0]
+    st, epi_r, done, times = startup() # 加速3-5个timestep
+    action_count = [0,0,times,0]
+
     while done is not True:
         st = torch.tensor(st.copy()).reshape(3,96,96).unsqueeze_(0).float()/255
         action = torch.argmax(net(st)).item()
-        if action == 2:
+        action_count[action] += 1 # 统计原始动作数量比例
+
+        if action == 2: # 不再加速
             action = 3
-        action_count[action] += 1
+        
+        if action_count[0] >= 10 and action == 0: # 纠正连续左转
+            st, rt, done, times = repeat()
+            action_count[3] += times
+            epi_r += rt
+            action_count[0] = 0
+            continue
+        elif action_count[1] >= 10 and action == 1: # 纠正连续右转
+            st, rt, done, times = repeat()
+            action_count[3] += times
+            epi_r += rt
+            action_count[1] = 0
+            continue
+        
         st, rt, done, _ = env.step(action_projection(action))
         epi_r += rt
-        env.render()
+        if render: env.render()
+
     print(epi_r,action_count)
-
-
+    r_list.append(epi_r)
 env.close()
+print('average episodic reward:',np.array(r_list).mean())
+
